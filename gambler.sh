b830373fcaf9943d45f4efbb5245f13ae8d439c2
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-13 16:01:33 +0530

    Add feature to show number of dollar win or loose

--- gambler.sh
+++ gambler.sh
@@ -1,17 +1,34 @@
-!/bin/bash -x                                                                  
-  betAmount=1
-  stakeValue=100
-  noOfdays=10
+#!/bin/bash -x                                                                  
+  bet=0
+  win=0
+  loose=0
+  bet1=0
+ finalstakeValue=0
+  read -p "enter number of days :" noOfdays
   read -p "enter number of game want to play :" noOfgame
-    for ((i=0; i<noOfdays ;i++))
-     do
-      while [ $stakeValue -gt 0 ]
-          do
-           for ((j=0;j<noOfgame;j++))
-             do
-               ((stakeValue--))
-            done
-         done
-     done
-
-
+    for ((i=0; i<$noOfdays ;i++))
+     do    
+        stakeValue=100
+         for ((j=0; j<noOfgame; j++))
+           do
+             while [ $bet1 -ne $noOfgame ]
+               do
+                bet=$((RANDOM%2))
+                 ((bet1++))
+                 if [ $bet -eq 1 ]
+                   then
+                    ((win++))
+                    ((stakeValue++))
+                    echo "after win" $stakeValue
+                    else
+                    ((lose++))
+                    ((stakeValue--))
+                    echo "after loose" $stakeValue
+                fi
+      done     
+    done
+    bet1=0
+    done
+     echo "number of dollar " $loose
+     echo "number of dollar win " $win
+     echo "final Stake value after game :" $stakeValue 
